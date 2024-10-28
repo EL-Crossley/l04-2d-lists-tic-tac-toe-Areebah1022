79a016916253365c3ff338e7a04ba63c28d12249
Areebah1022 <areebahmateen1@gmail.com>
2024-10-28 13:40:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 # Put your function here
+def check_winner(board):
+def ticTacToe(board):
+ 
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != ' ':
+            return board[i][0]  
+        if board[0][i] == board[1][i] == board[2][i] != ' ':
+            return board[0][i]  
 
+    if board[0][0] == board[1][1] == board[2][2] != ' ':
+        return board[0][0]  
+    if board[0][2] == board[1][1] == board[2][0] != ' ':
+        return board[0][2]  
+
+    if all(cell != ' ' for row in board for cell in row):
+        return 'TIE'
+
+    return ' '  
+board = [['X','O','O'],['O','X','O'],['X','O','X']]
+print(ticTacToe(board))
 
 # testing
 board = [['X','O','O'],['O','X','O'],['X','O','X']]
